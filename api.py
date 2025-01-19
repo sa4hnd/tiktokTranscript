d7b3c181293ca3f565c540d150b6d0ffcc203591
sa4hnd <shexhtc@gmail.com>
2025-01-19 17:51:28 +0300

    added url

--- api.py
+++ api.py
@@ -20,9 +20,12 @@ ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://localhost:8080",
     "http://localhost:5173",  # Vite default port
+    "http://localhost:8081",  # Added new localhost port
     "http://127.0.0.1:8080",
     "http://127.0.0.1:3000",
     "http://127.0.0.1:5173",
+    "http://127.0.0.1:8081",  # Added new localhost port
+    "https://nodeflowai.com",  # Added production domain
 ]
 
 # Setup AssemblyAI
